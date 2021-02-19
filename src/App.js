c24023bc153c2688423361182b78f4c31a0ac25e
MizanurRahmann <mr926560@gmail.com>
2021-02-19 19:27:30 +0600

    add routers.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,46 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import gsap from 'gsap';
 
-// Components
-import Header from './components/header';
-import Banner from './components/banner';
-import Cases from './components/cases';
-import IntroOverlay from './components/introOverlay';
-
-// Scss
+// scss
 import './styles/App.scss';
 
+// pages & components
+import Home from './pages/home';
+import CaseStudies from './pages/caseStudies';
+import Approach from './pages/approach';
+import Services from './pages/services';
+import About from './pages/about';
+import Header from './components/header';
+import { Route } from 'react-router-dom';
+
+// routes
+const routes = [
+  {path: '/', name: 'Home', component: Home},
+  {path: '/case-studies', name: 'Case Studies', component: CaseStudies},
+  {path: '/approach', name: 'Approach', component: Approach},
+  {path: '/services', name: 'Services', component: Services},
+  {path: '/about-us', name: 'About Us', component: About},
+];
+
+
+
 function App() {
-  const title = useRef(null);
-
   useEffect(() => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
     // prevent flashings
     gsap.to('body', 0, {css: {visibility: "visible"}});
-    
-    // timeline
-    const tl = gsap.timeline();
-
-    tl.from(".line span", 1.8, {
-      y: 100,
-      ease: "power4.out",
-      delay: 1,
-      skewY: 7,
-      stagger: {
-        amount: 0.3
-      }
-    }).to(".overlay-top", 1.6, {
-      height: 0,
-      ease: "expo.inOut",
-      stagger: 0.4
-    }).to(".overlay-bottom", 1.6, {
-      width: 0,
-      ease: "expo.inOut",
-      delay: -2,
-      stagger: {
-        amount: 0.4
-      }
-    }).to(".intro-overlay", 0,{
-      css: {display: "none"}
-    }).from(".case-image img", 1.6, {
-      scale: 1.4,
-      ease: "expo.inOut",
-      delay: -2,
-      stagger: {
-        amount: 0.4
-      }
-    })
-    
   }, [])
 
   return (
-    <div className="App">
-        <IntroOverlay />
+    <>
        <Header />
-       <Banner title={title} />
-       <Cases />
-    </div>
+       <div className="App">
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/services' component={Services} />
+          <Route exact path='/approach' component={Approach} />
+          <Route exact path='/case-study' component={CaseStudies} />
+       </div>
+    </>
   );
 }
 
